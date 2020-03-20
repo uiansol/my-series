db28afb77c690bab0b20c20c32e96f5610831051
Uian Sol <sol.uian@gmail.com>
2020-03-20 09:01:19 -0300

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap'
 
 function App () {
+  const [open, setOpen] = useState(false)
+
+  const toogle = () => {
+    setOpen(!open)
+  }
+
   return (
     <div>
-      <h1>My series</h1>
+      <Navbar color='light' light expand='md'>
+        <NavbarBrand>My series</NavbarBrand>
+        <NavbarToggler onClick={toogle} />
+        <Collapse isOpen={open} navbar>
+          <Nav className='m1-auto' navbar>
+            <NavItem>
+              <NavLink href='/'>Gender</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   )
 }
