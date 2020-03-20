ef69f26489bc514cd9c99ac3aa6333a58af38911
Uian Sol <sol.uian@gmail.com>
2020-03-20 17:30:24 -0300

    Finish CRUD of genres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Header from './Header'
 import Genres from './Genres'
+import NewGenre from './NewGenre'
+import EditGenre from './EditGenre'
+
 import axios from 'axios'
 import {
   BrowserRouter as Router,
@@ -24,7 +27,9 @@ function App () {
       <div>
         <Header />
         <Route path='/' exact component={Home} />
-        <Route path='/genres' component={Genres} />
+        <Route path='/genres' exact component={Genres} />
+        <Route path='/genres/:id' exact component={EditGenre} />
+        <Route path='/genres/new' exact component={NewGenre} />
       </div>
     </Router>
   )
--- src/Genres.js
+++ src/Genres.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const Genres = () => {
   const [data, setData] = useState([])
@@ -11,12 +12,24 @@ const Genres = () => {
       })
   }, [])
 
+  const deleteGenre = (id) => {
+    axios
+      .delete('/api/genres/' + id)
+      .then(res => {
+        const filtered = data.filter(item => item.id !== id)
+        setData(filtered)
+      })
+  }
+
   const renderLine = (record) => {
     return (
       <tr key={record.id}>
         <th scope='row'>{record.id}</th>
         <td>{record.name}</td>
-        <td><button>+</button></td>
+        <td>
+          <button className='btn btn-danger' onClick={() => deleteGenre(record.id)}>Remove</button>
+          <Link className='btn btn-warning' to={'/genres/' + record.id}>Edit</Link>
+        </td>
       </tr>
     )
   }
@@ -25,7 +38,7 @@ const Genres = () => {
     return (
       <div className='container'>
         <h1>Genres</h1>
-        <div class='alert alert-warning' role='alert'>
+        <div className='alert alert-warning' role='alert'>
           You don't have genres created.
         </div>
       </div>
@@ -35,6 +48,7 @@ const Genres = () => {
   return (
     <div className='container'>
       <h1>Genres</h1>
+      <div><Link className='btn btn-primary' to='/genres/new'>New genre</Link></div>
       <table className='table table-dark'>
         <thead>
           <tr>
